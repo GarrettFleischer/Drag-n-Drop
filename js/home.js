db60823c1514a75fdfc2ede518b8be11647e9d69
Garrett Fleischer <garrett.fleischer@oit.edu>
2017-04-05 00:47:14 -0700

    Added ability to drag a piece into another piece.

--- js/home.js
+++ js/home.js
@@ -2,13 +2,20 @@
  * Created by Garrett on 4/4/2017.
  */
 
-var dragSrcEl = null;
+var SrcType = {Piece: 0, LinePiece: 1};
+Object.freeze(SrcType);
 
+var dragSrcEl = null;
+var dragSrcType = SrcType.Piece;
+
+// TODO add blank pieces in between for placing new pieces
 function parsePieces(text) {
     var words = text.split(' ');
     var pieceWrapper = document.createElement('div');
     pieceWrapper.classList.add('line-piece-wrapper');
     pieceWrapper.setAttribute('draggable', true);
+    pieceWrapper.addEventListener('dragstart', handleDragStartLinePiece, true);
+    pieceWrapper.addEventListener('dragend', handleDragEnd, true);
 
     for (var i = 0; i < words.length; ++i) {
         var piece = null;
@@ -17,32 +24,49 @@ function parsePieces(text) {
             case '{':
             case '}':
                 piece = document.createElement('div');
+                piece.innerHTML = words[i];
                 break;
 
             case '...':
                 piece = document.createElement('span');
                 piece.classList.add('empty-piece');
+                piece.addEventListener('dragenter', handleDragEnter, false);
+                piece.addEventListener('dragover', handleDragOver, false);
+                piece.addEventListener('dragleave', handleDragLeave, false);
+                piece.addEventListener('drop', handleDropEmptyPiece, false);
+                piece.addEventListener('dragend', handleDragEnd, false);
                 break;
 
             default:
                 piece = document.createElement('span');
+                piece.innerHTML = words[i];
                 break;
         }
 
         piece.classList.add('line-piece');
-        piece.innerHTML = words[i];
         pieceWrapper.appendChild(piece);
     }
 
     return pieceWrapper;
 }
 
-function handleDragStart(e) {
+function handleDragStartPiece(e) {
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.innerHTML);
 
     this.style.opacity = '0.4';
     dragSrcEl = this;
+    dragSrcType = SrcType.Piece;
+}
+
+function handleDragStartLinePiece(e) {
+    console.log(this.outerHTML);
+    e.dataTransfer.effectAllowed = 'move';
+    e.stopPropagation();
+
+    this.style.opacity = '0.4';
+    dragSrcEl = this;
+    dragSrcType = SrcType.LinePiece;
 }
 
 function handleDragOver(e) {
@@ -60,18 +84,38 @@ function handleDragLeave(e) {
     this.classList.remove('over');
 }
 
-function handleDrop(e) {
+function handleDropLine(e) {
     if(e.stopPropagation) e.stopPropagation();
 
     if (dragSrcEl !== this) {
-        var data = e.dataTransfer.getData('text/html');
-        var temp = document.createElement('div');
-        temp.innerHTML = data;
-        if(!temp.classList.contains('line-piece'))
-            this.innerHTML = parsePieces(temp.innerHTML).outerHTML;
+        switch (dragSrcType) {
+            case SrcType.Piece:
+                var pieces = parsePieces(e.dataTransfer.getData('text/html'));
+                this.appendChild(pieces);
+                break;
+            case SrcType.LinePiece:
+                this.appendChild(dragSrcEl);
+                break;
+        }
     }
 
     return false;
+}
+
+function handleDropEmptyPiece(e) {
+    if(e.stopPropagation) e.stopPropagation();
+
+    if (dragSrcEl !== this) {
+        switch (dragSrcType) {
+            case SrcType.Piece:
+                var pieces = parsePieces(e.dataTransfer.getData('text/html'));
+                this.replaceWith(pieces);
+                break;
+            case SrcType.LinePiece:
+                this.replaceWith(dragSrcEl);
+                break;
+        }
+    }
 }
 
 function handleDragEnd(e) {
@@ -83,7 +127,7 @@ function handleDragEnd(e) {
 
 var pieces = document.querySelectorAll('.piece');
 [].forEach.call(pieces, function (piece) {
-    piece.addEventListener('dragstart', handleDragStart, false);
+    piece.addEventListener('dragstart', handleDragStartPiece, false);
     piece.addEventListener('dragend', handleDragEnd, false);
 });
 
@@ -92,7 +136,7 @@ var lines = document.querySelectorAll('.line');
     line.addEventListener('dragenter', handleDragEnter, false);
     line.addEventListener('dragover', handleDragOver, false);
     line.addEventListener('dragleave', handleDragLeave, false);
-    line.addEventListener('drop', handleDrop, false);
+    line.addEventListener('drop', handleDropLine, false);
     line.addEventListener('dragend', handleDragEnd, false);
 });
 
